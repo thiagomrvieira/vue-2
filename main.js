46c8a459090c5ae829cb851cc96e1b5cc3651fc4
Thiago Vieira <tmaciel@digital-works.com>
2022-06-04 10:46:33 +0100

    Components within components

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
+Vue.component('task-list', {
+    template: `
+        <ul> 
+            <task v-for="task in tasks"> {{ task.name }} </task>
+        </ul>
+    `,
+    data() {
+        return {
+            tasks: [
+                { name: 'Learn Vue' },
+                { name: 'Learn Vuex' },
+                { name: 'Learn Vue Router' }
+            ],
+        }
+    },
+    
+      
+});
+
 Vue.component('task', {
     template: `
-        <li>  <slot></slot> </li>
+        <li><slot></slot> </li>
     `
 });
+
+
 
 new Vue ({
     el: '#root'
