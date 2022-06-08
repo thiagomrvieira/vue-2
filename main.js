ab915121b66fca2177780145f00fc42259ffc18c
Thiago Vieira <tmaciel@digital-works.com>
2022-06-08 12:54:58 +0100

    Single-Use Components and Inline Templates

--- main.js
+++ main.js
@@ -1,36 +1,12 @@
-window.Event = new Vue();
 
+Vue.component('progress-view', {
 
-Vue.component('modal', {
-    template: `
-        <div class="modal is-active">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">
-                        <slot name="header"></slot>
-                    </p>
-                    <button class="delete" aria-label="close"></button>
-                </header>
-                <section class="modal-card-body">
-                    <slot></slot>
-                </section>
-                <footer class="modal-card-foot">
-                    <slot name="footer"></slot>
-                </footer>
-            </div>
-        </div>
-    
-    `,
-
-  
+    data() {
+        return {completionRate: 10};
+    },
    
 });
 
-
-
 new Vue ({
     el: '#root', 
-
-
 });
