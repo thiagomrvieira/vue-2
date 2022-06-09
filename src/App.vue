b4d927a6695ea533d01c84a05fa22c7ab766e23e
Thiago Vieira <tmaciel@digital-works.com>
2022-06-09 12:45:12 +0100

    Webpack and vue-cli

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<Message>
+			Hello World
+		</Message>
+
+		<Message>
+			Hey there
+		</Message>
+		
+	</div>
+</template>
+
+<script>
+import Message from './components/Message.vue'
+
+export default {
+    name: "app",
+    data() {
+        return {};
+    },
+    components: { Message }
+}
+</script>
+
+<style>
+
+</style>
